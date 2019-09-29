4fb42c53c8f9ccbc83c6128a793e23533cde4bf9
Naftali Lubin <naftalilubin@gmail.com>
2019-09-28 22:30:58 -0400

    Start work on site redesign

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled, { keyframes } from "styled-components";
+import headerImage from "./images/header.png";
 
 const AppHeader = styled.div`
-  background-color: ${(props) => props.backgroundColor};
-  height: 12rempx;
-  padding: 20px;
-  color: ${(props) => props.fontColor};
-  margin-bottom: 0rem;
-  
+  position: relative;
+  background-color: ${props => props.backgroundColor};
+  color: #fff;
+  margin-bottom: 0;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+
   h2 {
-    margin: 0;
     font-size: 3rem;
+    line-height: 3rem;
+    height: 3rem;
+    width: auto;
+    font-family: "Work Sans", sans-serif;
+    font-weight: 900;
+    font-style: normal;
+    margin: 0;
+    letter-spacing: 3px;
+    filter: none;
+
+    &:after {
+      content: ""; /* This is necessary for the pseudo element to work. */
+      display: block; /* This will put the pseudo element on its own line. */
+      margin: 0 auto; /* This will center the border. */
+      width: 25%; /* Change this to whatever width you want. */
+      padding-top: 1rem; /* This creates some space between the element and the border. */
+      border-bottom: 3px solid rgba(79, 255, 150, 0.34); /* This creates the border. Replace black with whatever color you want. */
+    }
   }
-  
   small {
-    font-size: 1rem;
+    font-size: 1.1rem;
+    letter-spacing: 2px;
+    margin-top: 2.5rem;
+  }
+
+  .after {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    filter: blur(2px);
+    background: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.55)),
+      url(${headerImage});
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
   }
 `;
+
 const HeaderLink = styled.a`
   text-decoration: none;
   font-weight: bold;
@@ -24,19 +64,27 @@ const HeaderLink = styled.a`
 `;
 const FlexContainer = styled.div`
   display: flex;
-  flex-direction: ${({flexDirection = 'row'}) => flexDirection};
+  flex-direction: ${({ flexDirection = "row" }) => flexDirection};
   flex-wrap: wrap;
   justify-content: center;
-  flex: ${({flex}) => flex};
-  min-height: ${({height}) => height};
+  flex: ${({ flex }) => flex};
+  min-height: ${({ height }) => height};
+`;
+const Page = styled(FlexContainer)`
+  width: 80vw;
+  margin: 0 auto;
 `;
 const FlexItem = styled.div`
-  order: ${(props) => props.order};
-  flex: ${(props) => props.flex};
-  flex-basis: ${(props) => props.flexBasis};
-  flex-shrink: ${(props) => props.flexShrink};
-  flex-grow: ${(props) => props.flexGrow};
-  align-self: ${(props) => props.alignSelf};
+  order: ${props => props.order};
+  flex: ${props => props.flex};
+  flex-basis: ${props => props.flexBasis};
+  flex-shrink: ${props => props.flexShrink};
+  flex-grow: ${props => props.flexGrow};
+  align-self: ${props => props.alignSelf};
+`;
+const fadeIn = keyframes`
+    from { opacity: 0; }
+    to   { opacity: 1; }
 `;
 const Card = styled(FlexItem)`
   border: 1px solid grey;
@@ -44,9 +92,11 @@ const Card = styled(FlexItem)`
   padding: 2rem;
   margin: 1rem 1rem;
   text-align: center;
-  min-width: ${({minWidth = 18}) => `${minWidth}rem`};
+  min-width: ${({ minWidth = 18 }) => `${minWidth}rem`};
+  opacity: 1;
+  animation: ${fadeIn} 2s;
 `;
-const CardLink = styled(HeaderLink)`
+const CardHeaderLink = styled(HeaderLink)`
   padding-bottom: 1rem;
   color: black;
 `;
@@ -54,7 +104,8 @@ const CardList = styled.ul`
   list-style: none;
   display: flex;
   padding: 0;
-  
+  flex-direction: column;
+
   li {
     margin: auto;
   }
@@ -63,45 +114,59 @@ const CardList = styled.ul`
 class App extends Component {
   render() {
     return (
-      <FlexContainer flexDirection="column" height="100vh">
+      <div>
         <AppHeader>
-          <h2>Neal Lubin</h2>
-          <small>Consider it dealt with</small>
+          <h2>Naftali Lubin</h2>
+          <small>Full stack Software Engineer</small>
+          <div className="after" />
         </AppHeader>
-        <FlexContainer flex="1">
-          <Card>
-            <CardLink href="http://ctrlitall.com">Neal on Stack Overflow</CardLink>
-            
-            <p>
-              <a href="https://stackoverflow.com/users/561731/neal">
-                <img
-                  src="https://stackoverflow.com/users/flair/561731.png?theme=clean"
-                  width="208" height="58"
-                  alt="profile for Neal at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
-                  title="profile for Neal at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
-                />
-              </a>
-            </p>
-          </Card>
-          <Card>
-            <CardLink href="/servable">servable</CardLink>
-    
-            <p>A "simple" observable implementation</p>
-          </Card>
-        </FlexContainer>
+        <Page flexDirection="column" height="100vh">
+          <FlexContainer>
+            <Card flexBasis="100vw">
+              <h2>Where I can be found:</h2>
+              <CardList>
+                <li>
+                  <CardHeaderLink href="http://ctrlitall.com">
+                    Neal on Stack Overflow
+                  </CardHeaderLink>
+                </li>
+                <li>
+                  <CardHeaderLink href="https://github.com/maniator">
+                    @maniator on github
+                  </CardHeaderLink>
+                </li>
+                <li>
+                  <CardHeaderLink href="https://www.linkedin.com/in/neallubin/">
+                    Naftali Lubin on LinkedIn
+                  </CardHeaderLink>
+                </li>
+              </CardList>
+            </Card>
+          </FlexContainer>
 
-        <FlexContainer>
-          <Card flexBasis="100vw">
-            <h3>Where I can be found:</h3>
-            <CardList>
-              <li><CardLink href="http://ctrlitall.com">Neal on Stack Overflow</CardLink></li>
-              <li><CardLink href="https://github.com/maniator">@maniator on github</CardLink></li>
-              <li><CardLink href="https://twitter.com/maniator">@maniator on twitter</CardLink></li>
-              <li><CardLink href="https://www.linkedin.com/in/neallubin/">Neal Lubin on LinkedIn</CardLink></li>
-            </CardList>
-          </Card>
-        </FlexContainer>
-      </FlexContainer>
+          <FlexContainer>
+            <Card flexBasis="100vw">
+              <h2> What do I do? </h2>
+              <div>
+                {" "}
+                I help to build scalable full stack applications with a focus on
+                the frontend.{" "}
+              </div>
+
+              <h3>Technical Skills</h3>
+              <div>
+                <h4>Languages </h4>
+                JavaScript, TypeScript, PHP, MySQL, HTML, CSS, Python, Ruby
+              </div>
+              <div>
+                <h4>Libraries </h4>
+                React, redux, Backbone, Marionette, Angular, Handlebars,
+                flow-typed,lodash, express, NodeJS, django, Symfony, Rails
+              </div>
+            </Card>
+          </FlexContainer>
+        </Page>
+      </div>
     );
   }
 }
