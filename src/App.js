3dc8861f61cc9c85a66552d5ebc1bdff225bf536
Naftali Lubin <maniator@users.noreply.github.com>
2021-07-07 16:43:48 +0000

    Update site text and links

--- src/App.js
+++ src/App.js
@@ -1,317 +1,122 @@
 import React, { Component } from "react";
-import styled, { keyframes } from "styled-components";
-import headerImage from "./images/header.png";
-
-const AppHeader = styled.div`
-  position: relative;
-  color: #fff;
-  margin-bottom: 0;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  text-transform: uppercase;
-  height: 100vh;
-  justify-content: center;
-
-  h2 {
-    font-size: 3rem;
-    line-height: 3rem;
-    height: 3rem;
-    max-width: 80vw;
-    font-weight: 900;
-    font-style: normal;
-    margin: 0 auto;
-    letter-spacing: 3px;
-    filter: none;
-
-    &:after {
-      content: ""; /* This is necessary for the pseudo element to work. */
-      display: block; /* This will put the pseudo element on its own line. */
-      margin: 0 auto; /* This will center the border. */
-      width: 200px; /* Change this to whatever width you want. */
-      padding-top: 1rem; /* This creates some space between the element and the border. */
-      border-bottom: 3px solid #2a7ae2; /* This creates the border. Replace black with whatever color you want. */
-    }
-  }
-  small {
-    font-size: 1.1rem;
-    letter-spacing: 2px;
-    max-width: 80vw;
-    margin: 2.5rem auto 0;
-  }
-
-  .after {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    top: 0;
-    left: 0;
-    filter: blur(2px);
-    background: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.55)),
-      url(${headerImage});
-    background-size: cover;
-    background-position: center;
-    z-index: -1;
-  }
-
-  @media only screen and (max-width: 600px) {
-    h2 {
-      font-size: 2rem;
-      line-height: 2rem;
-      height: 2rem;
-    }
-  }
-`;
-
-const HeaderLink = styled.a`
-  display: block;
-  padding-bottom: 1rem;
-  text-decoration: none;
-  color: #2a7ae2;
-
-  &:hover {
-    color: black;
-    text-decoration: underline;
-  }
-`;
-const FlexContainer = styled.div`
-  display: flex;
-  flex-direction: ${({ flexDirection = "row" }) => flexDirection};
-  flex-wrap: wrap;
-  justify-content: center;
-  flex: ${({ flex }) => flex};
-  min-height: ${({ height }) => height};
-`;
-const Page = styled.div`
-  * {
-    box-sizing: content-box;
-  }
-  font-family: "Work Sans", sans-serif;
-`;
-const PageInner = styled(FlexContainer)`
-  max-width: 750px;
-  margin: 36px auto;
-
-  @media only screen and (max-width: 600px) {
-    max-width: 90vw;
-  }
-`;
-const FlexItem = styled.div`
-  order: ${props => props.order};
-  flex: ${props => props.flex};
-  flex-basis: ${props => props.flexBasis};
-  flex-shrink: ${props => props.flexShrink};
-  flex-grow: ${props => props.flexGrow};
-  align-self: ${props => props.alignSelf};
-`;
-const fadeIn = keyframes`
-    from { opacity: 0; }
-    to   { opacity: 1; }
-`;
-const Card = styled(FlexItem)`
-  border-radius: 5px;
-  margin: 1.75rem 1rem;
-  text-align: center;
-  min-width: ${({ minWidth = 18 }) => `${minWidth}rem`};
-  opacity: 1;
-  animation: ${fadeIn} 2s;
-`;
-
-const CardList = styled.ul`
-  list-style: none;
-  display: flex;
-  padding: 0;
-  flex-direction: column;
-  margin-bottom: 0;
-
-  li {
-    margin: auto;
-    &:last-child {
-      a {
-        padding: 0;
-      }
-    }
-  }
-`;
-const CardHeader = styled.h2`
-  text-transform: uppercase;
-  font-weight: 700;
-  margin: 0 0 1rem;
-
-  &:after {
-    content: ""; /* This is necessary for the pseudo element to work. */
-    display: block; /* This will put the pseudo element on its own line. */
-    margin: 0 auto; /* This will center the border. */
-    width: ${props =>
-      props.lineWidth || "150px"}; /* Change this to whatever width you want. */
-    padding-top: 0.5rem; /* This creates some space between the element and the border. */
-    border-bottom: 3px solid #9d9fa2; /* This creates the border. Replace black with whatever color you want. */
-  }
-`;
-
-const CardSmallHeader = styled.h3`
-  font-weight: 500;
-`;
-
-const TypeLogo = styled.i`
-  font-size: 48px;
-  width: 48px;
-  height: 48px;
-  color: #2a7ae2;
-`;
-
-const TypeLogoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0 4px;
-  padding: 8px;
-  font-size: 12px;
-
-  ${TypeLogo} {
-    margin: 0 auto 12px;
-  }
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-
-  input,
-  textarea,
-  button {
-    display: block;
-    width: 300px;
-    margin: 0 auto 8px;
-    border: 1px solid #9d9fa2;
-    height: 24px;
-    padding: 8px 16px;
-    font-size: 16px;
-  }
-
-  textarea {
-    min-height: 44px;
-  }
-
-  button {
-    background-color: #2a7ae2;
-    color: white;
-    width: 150px;
-    margin-bottom: 0;
-  }
-`;
+import * as C from "./components";
 
 class App extends Component {
   render() {
     return (
-      <Page>
-        <AppHeader>
+      <C.Page>
+        <C.AppHeader>
           <h2>Naftali Lubin</h2>
-          <small>Full stack Software Engineer</small>
+          <small>Full Stack Software Engineer</small>
           <div className="after" />
-        </AppHeader>
-        <PageInner flexDirection="column" height="100vh">
-          <FlexContainer>
-            <Card flexBasis="100vw">
-              <CardHeader>Where I can be found</CardHeader>
-              <CardList>
+        </C.AppHeader>
+        <C.PageInner flexDirection="column" height="100vh">
+          <C.FlexContainer>
+            <C.Card flexBasis="100vw">
+              <C.CardHeader>Where I can be found</C.CardHeader>
+              <C.CardList>
                 <li>
-                  <HeaderLink href="https://github.com/maniator">
-                    @maniator on Github
-                  </HeaderLink>
+                  <C.HeaderLink href="https://github.com/maniator">
+                    Github
+                  </C.HeaderLink>
                 </li>
                 <li>
-                  <HeaderLink href="http://ctrlitall.com">
-                    Neal on Stack Overflow
-                  </HeaderLink>
+                  <C.HeaderLink href="http://ctrlitall.com">
+                    Stack Overflow
+                  </C.HeaderLink>
                 </li>
                 <li>
-                  <HeaderLink href="https://www.linkedin.com/in/neallubin/">
-                    Naftali Lubin on LinkedIn
-                  </HeaderLink>
+                  <C.HeaderLink href="https://www.linkedin.com/in/naftalilubin/">
+                    LinkedIn
+                  </C.HeaderLink>
                 </li>
-              </CardList>
-            </Card>
-            <Card flexBasis="100vw">
-              <CardHeader> What I do </CardHeader>
+              </C.CardList>
+            </C.Card>
+            <C.Card flexBasis="100vw">
+              <C.CardHeader> What I do </C.CardHeader>
 
               <div>
-                I help to build scalable full stack applications with a focus on
-                the frontend.
+                I build scalable full stack web applications <br />
+                Father of three amazing boys <br />
+                Husband of one incredible wife
               </div>
-            </Card>
-            <Card flexBasis="100vw">
-              <CardHeader> Technical Skills </CardHeader>
+            </C.Card>
+            <C.Card flexBasis="100vw">
+              <C.CardHeader> Technical Skills </C.CardHeader>
 
               <div>
-                <CardSmallHeader>Languages </CardSmallHeader>
+                <C.CardSmallHeader>Languages </C.CardSmallHeader>
 
                 {/* TODO Put names under all logos (and download the images (svgs) for use on site in the future */}
-                <FlexContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-javascript-plain" />
+                <C.FlexContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-javascript-plain" />
                     JavaScript
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-nodejs-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-nodejs-plain" />
                     NodeJS
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-typescript-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-typescript-plain" />
                     TypeScript
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-html5-plain" />
-                    HTML
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-css3-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-css3-plain" />
                     CSS
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-mysql-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-mysql-plain" />
                     MySQL
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-ruby-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-ruby-plain" />
                     Ruby
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-php-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-php-plain" />
                     PHP
-                  </TypeLogoContainer>
-                  <TypeLogoContainer>
-                    <TypeLogo className="devicon-python-plain" />
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-python-plain" />
                     Python
-                  </TypeLogoContainer>
-                </FlexContainer>
+                  </C.TypeLogoContainer>
+                  <C.TypeLogoContainer>
+                    <C.TypeLogo className="devicon-go-plain" />
+                    GoLang
+                  </C.TypeLogoContainer>
+                </C.FlexContainer>
               </div>
               <br />
               <div>
-                <CardSmallHeader> Libraries </CardSmallHeader>
-                React, Redux, Backbone, Marionette, Angular, Handlebars,
-                Flow-typed, Express, Django, Symfony, Rails
+                <C.CardSmallHeader> Libraries </C.CardSmallHeader>
+                React, Redux, Rails, Angular, Handlebars, Express, Django,
+                Symfony, Backbone, Marionette
               </div>
-            </Card>
-            <Card flexBasis="100vw">
-              <CardHeader>Projects</CardHeader>
-              <CardList>
+            </C.Card>
+            <C.Card flexBasis="100vw">
+              <C.CardHeader>Projects</C.CardHeader>
+              <C.CardListVertical>
                 <li>
-                  <HeaderLink href="https://github.com/maniator/servable">
+                  <C.HeaderLink href="https://github.com/maniator/servable">
                     servable - a simple observable
-                  </HeaderLink>
+                  </C.HeaderLink>
                 </li>
                 <li>
-                  <HeaderLink href="https://github.com/maniator/praetbot">
+                  <C.HeaderLink href="https://github.com/maniator/praetbot">
                     praetbot - a slackbot built in node
-                  </HeaderLink>
+                  </C.HeaderLink>
                 </li>
-              </CardList>
-            </Card>
-            <Card flexBasis="100vw">
-              <CardHeader>Get in touch</CardHeader>
-              <Form
+                <li>
+                  <C.HeaderLink href="https://blipit.net">
+                    Self playing/talking online baseball game
+                  </C.HeaderLink>
+                </li>
+              </C.CardListVertical>
+            </C.Card>
+            <C.Card flexBasis="100vw">
+              <C.CardHeader>Get in touch</C.CardHeader>
+              <C.Form
                 id="contactForm"
                 method="POST"
                 action="https://formspree.io/f/mqkgyjpr"
@@ -328,11 +133,11 @@ class App extends Component {
                 >
                   Send
                 </button>
-              </Form>
-            </Card>
-          </FlexContainer>
-        </PageInner>
-      </Page>
+              </C.Form>
+            </C.Card>
+          </C.FlexContainer>
+        </C.PageInner>
+      </C.Page>
     );
   }
 }
---
+++ src/components.js
@@ -0,0 +1,211 @@
+import styled, { keyframes } from "styled-components";
+import headerImage from "./images/header.png";
+
+export const AppHeader = styled.div`
+  position: relative;
+  color: #fff;
+  margin-bottom: 0;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  height: 100vh;
+  justify-content: center;
+
+  h2 {
+    font-size: 3rem;
+    line-height: 3rem;
+    height: 3rem;
+    max-width: 80vw;
+    font-weight: 900;
+    font-style: normal;
+    margin: 0 auto;
+    letter-spacing: 3px;
+    filter: none;
+
+    &:after {
+      content: ""; /* This is necessary for the pseudo element to work. */
+      display: block; /* This will put the pseudo element on its own line. */
+      margin: 0 auto; /* This will center the border. */
+      width: 200px; /* Change this to whatever width you want. */
+      padding-top: 1rem; /* This creates some space between the element and the border. */
+      border-bottom: 3px solid #2a7ae2; /* This creates the border. Replace black with whatever color you want. */
+    }
+  }
+  small {
+    font-size: 1.1rem;
+    letter-spacing: 2px;
+    max-width: 80vw;
+    margin: 2.5rem auto 0;
+  }
+
+  .after {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    filter: blur(2px);
+    background: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.55)),
+      url(${headerImage});
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
+  }
+
+  @media only screen and (max-width: 600px) {
+    h2 {
+      font-size: 2rem;
+      line-height: 2rem;
+      height: 2rem;
+    }
+  }
+`;
+
+export const HeaderLink = styled.a`
+  display: block;
+  padding-bottom: 1rem;
+  text-decoration: none;
+  color: #2a7ae2;
+
+  &:hover {
+    color: black;
+    text-decoration: underline;
+  }
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: ${({ flexDirection = "row" }) => flexDirection};
+  flex-wrap: wrap;
+  justify-content: center;
+  flex: ${({ flex }) => flex};
+  min-height: ${({ height }) => height};
+`;
+
+export const Page = styled.div`
+  * {
+    box-sizing: content-box;
+  }
+  font-family: "Work Sans", sans-serif;
+`;
+
+export const PageInner = styled(FlexContainer)`
+  max-width: 750px;
+  margin: 36px auto;
+
+  @media only screen and (max-width: 600px) {
+    max-width: 90vw;
+  }
+`;
+
+export const FlexItem = styled.div`
+  order: ${props => props.order};
+  flex: ${props => props.flex};
+  flex-basis: ${props => props.flexBasis};
+  flex-shrink: ${props => props.flexShrink};
+  flex-grow: ${props => props.flexGrow};
+  align-self: ${props => props.alignSelf};
+`;
+
+const fadeIn = keyframes`
+    from { opacity: 0; }
+    to   { opacity: 1; }
+`;
+export const Card = styled(FlexItem)`
+  border-radius: 5px;
+  margin: 1.75rem 1rem;
+  text-align: center;
+  min-width: ${({ minWidth = 18 }) => `${minWidth}rem`};
+  opacity: 1;
+  animation: ${fadeIn} 2s;
+`;
+
+export const CardList = styled.ul`
+  list-style: none;
+  display: flex;
+  padding: 0;
+  flex-direction: row;
+  margin-bottom: 0;
+
+  li {
+    margin: auto;
+  }
+`;
+
+export const CardListVertical = styled(CardList)`
+  flex-direction: column;
+
+  li:last-child a {
+    padding: 0;
+  }
+`;
+
+export const CardHeader = styled.h2`
+  text-transform: uppercase;
+  font-weight: 700;
+  margin: 0 0 1rem;
+
+  &:after {
+    content: ""; /* This is necessary for the pseudo element to work. */
+    display: block; /* This will put the pseudo element on its own line. */
+    margin: 0 auto; /* This will center the border. */
+    width: ${props =>
+      props.lineWidth || "150px"}; /* Change this to whatever width you want. */
+    padding-top: 0.5rem; /* This creates some space between the element and the border. */
+    border-bottom: 3px solid #9d9fa2; /* This creates the border. Replace black with whatever color you want. */
+  }
+`;
+
+export const CardSmallHeader = styled.h3`
+  font-weight: 500;
+`;
+
+export const TypeLogo = styled.i`
+  font-size: 48px;
+  width: 48px;
+  height: 48px;
+  color: #2a7ae2;
+`;
+
+export const TypeLogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 4px;
+  padding: 8px;
+  font-size: 12px;
+
+  ${TypeLogo} {
+    margin: 0 auto 12px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  input,
+  textarea,
+  button {
+    display: block;
+    width: 300px;
+    margin: 0 auto 8px;
+    border: 1px solid #9d9fa2;
+    height: 24px;
+    padding: 8px 16px;
+    font-size: 16px;
+  }
+
+  textarea {
+    min-height: 44px;
+  }
+
+  button {
+    background-color: #2a7ae2;
+    color: white;
+    width: 150px;
+    margin-bottom: 0;
+  }
+`;
