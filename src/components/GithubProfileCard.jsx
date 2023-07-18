346d72014896948bb36ee6fdd0b0fd1b2c04b881
Naftali Lubin <naftalilubin@gmail.com>
2023-07-18 14:22:18 -0400

    update: add correct size to default github profile card

--- src/components/GithubProfileCard.jsx
+++ src/components/GithubProfileCard.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const ProfileCard = () => {
   return (
     <>
-      <div className="github-card" data-user="maniator" />
+      <div
+        className="github-card"
+        data-github="maniator"
+        data-width="400"
+        data-height="152"
+        data-theme="default"
+      />
       <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js" />
     </>
   );
