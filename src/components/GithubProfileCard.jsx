ce5e938db7164f5636c868cd92e5f29f1771dfd8
Naftali Lubin <naftalilubin@gmail.com>
2023-07-18 14:16:58 -0400

    update: size of github card

--- src/components/GithubProfileCard.jsx
+++ src/components/GithubProfileCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProfileCard = () => {
   return (
     <>
-      <div className="github-card" data-user="maniator" />
+      <div className="github-card" data-user="maniator" data-width="400" data-height="318" data-theme="medium" />
       <script src="//cdn.jsdelivr.net/gh/lepture/github-cards@latest/jsdelivr/widget.js" />
     </>
   );
