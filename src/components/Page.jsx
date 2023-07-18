0e0fe426234aca7a5a8a8d414b0300f4c9a4548d
Naftali Lubin <naftalilubin@gmail.com>
2023-07-18 14:26:11 -0400

    feat: preload github profile js script

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -12,7 +12,7 @@ function Page({ children, footer, ...props }) {
       <GlobalStyles />
       <SEO {...props} />
       <Helmet>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="//fonts.gstatic.com" crossOrigin />
 
         <link
           rel="preload"
@@ -24,6 +24,11 @@ function Page({ children, footer, ...props }) {
           href="//cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
           as="style"
         />
+        <link
+          rel="preload"
+          href="//cdn.jsdelivr.net/github-cards/latest/widget.js"
+          as="script"
+        />
 
         <link
           rel="stylesheet"
@@ -33,6 +38,7 @@ function Page({ children, footer, ...props }) {
           rel="stylesheet"
           href="//cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
         />
+        <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js" />
       </Helmet>
       <Header />
       <C.Main>{children}</C.Main>
